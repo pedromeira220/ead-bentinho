90aa7a5274342c77bbea478995fc406061e83fdd
Pedro Meira <pedrohmeira@icloud.com>
2023-08-16 08:41:38 -0300

    segunda aula

---
+++ cad-usuario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EAD</title>
+    <link rel="stylesheet" href="css/cad-usuario.css">
+</head>
+<body>
+    <div id="main">
+        <section>
+        <h2>EAD Cadastro</h2>
+            <form action="#">
+                <input type="text" name="usuario" class="campo" placeholder="Usuário" required>
+                <input type="text" name="email" class="campo" placeholder="E-mail" required>
+                <input type="password" name="senha" class="campo" placeholder="Senha" required>
+                <input type="password" name="redigite-senha" class="campo" placeholder="Redigite Senha" required>
+                <button>Cadastrar</button>
+            </form>
+            <p>ou</p>
+            <a href="index.php">Voltar</a>
+        </section>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EAD</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div id="main">
+        <section>
+        <h2>EAD Login</h2>
+            <form action="#">
+                <input type="text" name="usuario" class="campo" placeholder="E-MAIL" required>
+                <input type="password" name="senha" class="campo" placeholder="SENHA" required>
+                <button>ENTRAR</button>
+            </form>
+            <p>ou</p>
+            <a href="cad-usuario.php">CRIE UMA CONTA</a>
+        </section>
+    </div>
+
+</body>
+</html>
